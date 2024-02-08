76e610772cda6a7c23466470466b0908e4411bcd
Jirka Tesar <jtesar@redhat.com>
2024-02-08 10:10:49 -0500

    dd

--- content/web4/modules/contrib/red_hat_shared_libs/dist/rhds-elements/modules/rh-global-footer-entry.js
+++
@@ -1,1 +0,0 @@
-import"./property-1.3.0.js";export{a as RhGlobalFooter}from"./rh-global-footer-1.3.0.js";import"./slot-controller-1.3.0.js";import"./logger-1.3.0.js";import"./class-map-1.3.0.js";import"./provider-1.3.0.js";import"./consumer-1.3.0.js";
